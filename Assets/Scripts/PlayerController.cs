b2f001d6196db401ac11e5093df7770cf8531948
Inno <enoatoz6@gmail.com>
2025-02-02 12:28:47 -0500

    Added script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float moveSpeed;
+    private Vector2 movement;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+       
+    }
+
+    private void FixedUpdate()
+    {
         
+        transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);  
+
     }
 }
